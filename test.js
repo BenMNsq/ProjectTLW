05455d8a1f96adeb55934b744fdd9b734f647855
jeanne <jeanne.claret@cpe.fr>
2021-10-05 12:16:46 +0200

    carousel

--- test.js
+++ test.js
@@ -4,3 +4,12 @@ let profile = {
     email : "benjaminsaaque@gmail.com",
     DateOfBirth : "31/05/2001",
 }
+console.log("variable");{} */ Permet de débug car sert de "print" */
+
+let btn=document.getElementById("btn");
+btn.onclick=function(event) {
+    console.log("j'ai été cliqué");
+}
+btn.addEventListener('click',function(event){
+    console.log("deuxieme fonction!");
+});
